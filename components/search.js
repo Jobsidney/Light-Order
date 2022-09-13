86bace77984c90747edef5db046b1b87e7e2d847
Jobsidney <jobsidney67@gmail.com>
2022-09-13 11:53:04 +0300

    Next js file

---
+++ components/search.js
@@ -0,0 +1,18 @@
+function Search() {
+  return (
+    <div className="search all">
+        <div className="search">
+            <div>logo</div>
+            <input className='border-y-2 border-x-2 rounded-3xl outline-transparent bg-slate-400 px-3' type="text" />
+            <div className=" border-y-2 border-x-2 rounded-3xl outline-transparent bg-slate-400 px-3 w-48"></div>
+        </div>
+        <div className="user search">
+            <div>dd</div>
+            <div>dd</div>
+            <div>dd</div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
