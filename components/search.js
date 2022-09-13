45514e2d7e4f5498a9499a71970898f51fc1cd28
Jobsidney <jobsidney67@gmail.com>
2022-09-13 12:03:16 +0300

    Search style

--- components/search.js
+++ components/search.js
@@ -3,8 +3,8 @@ function Search() {
     <div className="search all">
         <div className="search">
             <div>logo</div>
-            <input className='border-y-2 border-x-2 rounded-3xl outline-transparent bg-slate-400 px-3' type="text" />
-            <div className=" border-y-2 border-x-2 rounded-3xl outline-transparent bg-slate-400 px-3 w-48"></div>
+            <input className='bg-slate-400' type="text" />
+            <div className="currency outline-transparent bg-slate-400 px-3"></div>
         </div>
         <div className="user search">
             <div>dd</div>
