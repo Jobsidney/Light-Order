7b6f7c64543f9782d553fa292488abbdc023a87e
Jobsidney <jobsidney67@gmail.com>
2022-09-20 14:03:46 +0300

    Floating card

---
+++ components/navBar/IntroInfo.js
@@ -0,0 +1,22 @@
+function IntroInfo() {
+  return (
+    <div>
+        <div className="hero" id="home">
+        <div className="heroSec">
+            <div className="content">
+                <h1>Find your desired Product!</h1>
+                <h2>What do you desire?</h2>
+                <p>We ought to be about something beyond selling. We ought to be a piece of our client’s lives
+                    and the networks in which we serve and deliver</p>
+                {/* <Button word={'Explore More'}/> */}
+            </div>
+            <div className="image">
+                <img src={"/../images/cart.png"} alt="" width="900"/>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default IntroInfo
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Items from '../components/ItemsBar/Items'
+import IntroInfo from '../components/navBar/IntroInfo'
 import NavBar from '../components/navBar/NavBar'
 import Search from '../components/SearchBar/search'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <NavBar/>
-      
+    <div >
+      {/* <div className='opacity'>
+      </div> */}
+      <div className="wholeINtro">
+          <NavBar/>
+          <IntroInfo/>
+        </div>
+        <div className="descr">
+          <div className='floating-card'>
+            <h1>Why Light Order?</h1>
+            <p>Light Order operates its retail work on a stand-alone basis</p>
+            <div>
+              
+            </div>
+
+          </div>
+        </div>
     </div>
   )
 }
