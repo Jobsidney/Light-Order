dd7a32e853b4fa8c62c238cb37e7d989246a31d6
Jobsidney <jobsidney67@gmail.com>
2022-09-21 10:51:03 +0300

    Before and after product section

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,23 @@ import Search from '../components/SearchBar/search'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  function Title({title,des}){
+    return(
+      <div>
+            <h1>{title}</h1>
+            <p>{des}</p>
+      </div>
+    )
+  }
   return (
     <div className='body'>
-      {/* <div className='opacity'>
-      </div> */}
       <div className="wholeINtro">
           <NavBar/>
           <IntroInfo/>
         </div>
         <div className="descr">
           <div className='floating-card'>
-            <h1>Why Light Order?</h1>
-            <p>Light Order operates its retail work on a stand-alone basis</p>
+            <Title title="Why Light Order?" des="Light Order operates its retail work on a stand-alone basis"/>
             <div className='inline-flex'>
               <FloatCard/>
               <FloatCard/>
@@ -29,8 +34,10 @@ export default function Home() {
           </div>
         </div>
         <div className="products">
-          <h2>Our Best Product</h2>
-          <p>open-sell environment features an ever-increasing amount</p>
+          <Title title={"Our Best Product"} des={"open-sell environment features an ever-increasing amount"}/>
+          <div className="itemsCard">
+            <div></div>
+          </div>
         </div>
     </div>
   )
