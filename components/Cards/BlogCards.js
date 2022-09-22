0237dc6c7cd2c6d6ca1ae30432695b2426dad969
Jobsidney <jobsidney67@gmail.com>
2022-09-23 00:09:50 +0300

    span design

---
+++ components/Cards/BlogCards.js
@@ -0,0 +1,12 @@
+function BlogCards() {
+  return (
+    <div className='fcard m-5 flex flex-col'>
+          <img src={"/../images/cart.png"} alt=""/> 
+            <h4>Npure</h4>
+            <p>Cico Beat The sun Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut sapiente pariatur quibusdam obcaecati quisquam possimus </p>
+            <button>More Details </button>
+    </div>
+  )
+}
+
+export default BlogCards
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import AdvertCard from '../components/Cards/AdvertCard'
+import BlogCards from '../components/Cards/BlogCards'
 import FloatCard from '../components/Cards/FloatCard'
 import Items from '../components/ItemsBar/Items'
 import IntroInfo from '../components/navBar/IntroInfo'
@@ -9,10 +10,10 @@ import Search from '../components/SearchBar/search'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  function Title({title,des}){
+  function Title({title,span,span2,des}){
     return(
       <div>
-            <h1>{title}</h1>
+            <h1><span>{span2}</span>{title}<span>{span}</span></h1>
             <p>{des}</p>
       </div>
     )
@@ -25,7 +26,7 @@ export default function Home() {
         </div>
         <div className="descr">
           <div className='floating-card'>
-            <Title title="Why Light Order?" des="Light Order operates its retail work on a stand-alone basis"/>
+            <Title title="Why " span={'Light Order?'} des="Light Order operates its retail work on a stand-alone basis"/>
             <div className='inline-flex'>
               <FloatCard/>
               <FloatCard/>
@@ -35,17 +36,24 @@ export default function Home() {
           </div>
         </div>
         <div className="products">
-          <Title title={"Our Best Product"} des={"open-sell environment features an ever-increasing amount"}/>
+          <Title title={"Our Best "} span="Product" des={"open-sell environment features an ever-increasing amount"}/>
           <div className="itemsCard flex justify-center">
+            <AdvertCard/>
             <AdvertCard/>
             <AdvertCard/>
             <AdvertCard/>
           </div>
         </div>
         <div className="blogs text-center">
-          <Title title="Why Light Order?" des="Light Order operates its retail work on a stand-alone basis"/>
-          <div className="">
-            <div className='fcard'></div>
+          <Title title="Our Blog" span2={'Read '} des="Light Order operates its retail work on a stand-alone basis"/>
+          <div className="flex justify-center">
+            <BlogCards/>
+            <BlogCards/>
+            <BlogCards/>
+          </div>
+          <div className="reviews">
+            <p><span>10,000+</span> trusted reviews</p>
+            <h2>by customers</h2>
           </div>
 
         </div>
