4dfd8ad3ef21b788dd40fbf8fe57eab5d7075b08
Jobsidney <jobsidney67@gmail.com>
2022-10-08 16:48:57 +0300

    Product section done

--- components/newBlog/Blogy.js
+++ components/newBlog/Blogy.js
@@ -5,6 +5,8 @@ import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 function Blogy() {
 
+    const arr=['Sports','hello','Electronics','Fashion','Computer','Phones','Food']
+
     const sliderLeft=()=>{
         let slider=document.getElementById('slider')
         slider.scrollLeft=slider.scrollLeft-400
@@ -16,26 +18,26 @@ function Blogy() {
 
     function MealCard(){
         return (
-    <div class="meal">
-    <img src="https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full-750x500.jpeg" class="meal-img" alt="Japanese Gyozas"/>
-    <div class="meal-content">
-        <div class="meal-tags">
-            <span class="tag tag--vegetarian">Vegetables</span>
+    <div className="meal">
+    <img src="https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full-750x500.jpeg" className="meal-img" alt="Japanese Gyozas"/>
+    <div className="meal-content">
+        <div className="meal-tags">
+            <span className="tag tag--vegetarian">Vegetables</span>
         </div>
-        <p class="meal-title">Japanese Gyozas</p>
+        <p className="meal-title">Japanese Gyozas</p>
 
-        <ul class="meal-attributes">
-            <li class="meal-attribute">
-                <IonIcon.IoRestaurantOutline class="meal-icon" name="flame-outline"/>
+        <ul className="meal-attributes">
+            <li className="meal-attribute">
+                <IonIcon.IoRestaurantOutline className="meal-icon" name="flame-outline"/>
                
                 <span><strong>100%</strong> organic</span>
             </li>
-            <li class="meal-attribute">
-                <Gi.GiStrongMan class="meal-icon" name="restaurant-outline"/>
+            <li className="meal-attribute">
+                <Gi.GiStrongMan className="meal-icon" name="restaurant-outline"/>
                 <span>NutriScore <strong>74</strong></span>
             </li>
-            <li class="meal-attribute">
-                <IonIcon.IoStarOutline class="meal-icon" name="star-outline"/>
+            <li className="meal-attribute">
+                <IonIcon.IoStarOutline className="meal-icon" name="star-outline"/>
                 <span>4.9 rating <strong>(537)</strong></span>
             </li>
         </ul>
@@ -44,17 +46,28 @@ function Blogy() {
         )
     }
   return (
-    <div className="product inline-block overflow-x-scroll scrollbar-hide flex flex-row align-middle">
-    <MdChevronLeft className='opac cursor-pointer' onClick={sliderLeft} size={70}/>
-        <div id="slider" class="flex flex-row scrollbar-hide overflow-x-scroll scroll white-space-nowrap scroll-smooth ">
+    <div className="product inline-block ">
+        <div id="slider" className="flex flex-row scrollbar-hide overflow-x-scroll scroll white-space-nowrap scroll-smooth ">
             <MealCard/>
             <MealCard/>
-            <MealCard/>
-            <MealCard/>
-            <MealCard/>
-            <MealCard/>
+            <div className="diets">
+                    <h3 className="heading-tertiary">We Work with any Item:</h3>
+                    <ul className="list">
+                        {
+                         arr.map(item=>{
+                            return(
+                                <li className="list-item">
+                                    <IonIcon.IoCheckmarkOutline className="list-icon" name="checkmark-outline"/><span>{item}</span>
+                                </li>
+                            )
+                         })   
+                        }
+                    </ul>
+                </div>
         </div>
-    <MdChevronRight className='opac cursor-pointer' onClick={sliderRight} size={70}/>
+        <div className="container all-recipes">
+                <a href="#" className="link">See all Products &rarr; </a>
+            </div>
     </div>
   )
 }
