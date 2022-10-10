a8629641b89560e65b766478987b22f971df1f8a
Jobsidney <jobsidney67@gmail.com>
2022-10-10 12:37:06 +0300

    info

--- client/pages/Dashbord/ItemsAll/index.js
+++ client/pages/Dashbord/ItemsAll/index.js
@@ -1,6 +1,9 @@
+import * as IoIcons from 'react-icons/io'
 import DashCard from "./DashCard"
 
 function Dashbord() {
+
+ const profile=['name',"username","Favourite",'Area of residence','Age']
   return (
     <div className="userItems ">
       <div className=" profile2 flex flex-row">
@@ -9,11 +12,25 @@ function Dashbord() {
         <DashCard/>
         <DashCard/>
       </div>
-      <div className=" flex profile  flex-row justify-between">
-        <div className="first ">
-        <h1>My Profile</h1>
+      <div className=" flex profile  flex-row justify-between ">
+        <div className="first mr-4 px-4 py-8">
+        <h1 className='text-center name underline'>My Profile</h1>
+        <ul className='flex flex-col pt-10'>
+
+          {
+            profile.map((item)=> {return (
+              <li className='flex flex-row mb-4 text-center inline-block align-baseline'>
+              <IoIcons.IoMdPerson size={30}/>
+              <span >{item}</span>
+          </li>
+            )})
+          }
+
+
+
+        </ul>
         </div>
-        <div className="second ">
+        <div className="second px-4 py-8 ml-4 ">
 
         </div>
       </div>
