73ebc16033b4c6f93d4c88785f2e1272a884979e
Jobsidney <jobsidney67@gmail.com>
2022-10-11 08:30:58 +0300

    FOrm

---
+++ client/pages/Authentication/Form.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Login from './Login'
+import login from './login.css'
+import SignUp from './SignUp'
+function Form({data}) {
+  
+  return (
+    <div className="image2">
+        <div className='overlay'>
+          <div className="all">
+          {data}
+          </div>
+         </div>
+    </div>
+    
+  )
+}
+
+export default Form
